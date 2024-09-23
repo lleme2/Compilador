a4fbcab6281cf7a334028d2e44d6147e1a130377
ripercario <88801928+ripercario@users.noreply.github.com>
2024-09-23 16:32:07 -0300

    Base Analisador Sintatico
    Base da implementação do analisador sintatico retirando o loop do pega token fazendo com que a chamada da função pegue um token por vez

--- Compiler.cpp
+++ Compiler.cpp
@@ -1,54 +1,19 @@
 #include <stdio.h>
 #include <iostream>
-#include <queue>
 #include <iomanip>
 #include <string>
-#include <iomanip>
 #include <cctype>
-#include <algorithm>
 
 using namespace std;
 
 int contador = 1;
 
-struct ListaDeTokens {
-    queue<string> lexema;
-    queue<string> simbolo;
+struct Token {
+    string lexema;
+    string simbolo;
 };
 
-void imprime_lista_token(ListaDeTokens lista_de_tokens) {
-
-    cout << endl << endl << endl;
-
-    // Obtendo os tamanhos das filas
-    int tamanho_lexema = lista_de_tokens.lexema.size();
-    int tamanho_simbolo = lista_de_tokens.simbolo.size();
-
-    // Garantindo que as filas t�m o mesmo tamanho
-    if (tamanho_lexema != tamanho_simbolo) {
-        cerr << "Erro: Tamanho de 'lexema' e 'simbolo' s�o diferentes!" << endl;
-        return;
-    }
-
-    cout << "| " << setw(20) << left << "Lexema" << " | " << setw(20) << left << "Simbolo" << " |" << endl;
-    cout << "|----------------------|----------------------|" << endl;
-
-    while (!lista_de_tokens.lexema.empty() && !lista_de_tokens.simbolo.empty()) {
-        string lexema_elemento = lista_de_tokens.lexema.front(); // Pega o primeiro elemento de 'lexema'
-        string simbolo_elemento = lista_de_tokens.simbolo.front(); // Pega o primeiro elemento de 'simbolo'
-
-        cout << "| " << setw(20) << left << lexema_elemento << " | " << setw(20) << left << simbolo_elemento << " |" << endl;
-
-        lista_de_tokens.lexema.pop(); // Remove o elemento de 'lexema'
-        lista_de_tokens.simbolo.pop(); // Remove o elemento de 'simbolo'
-    }
-    cout << "|----------------------|----------------------|" << endl;
-}
-
-
-
-
-void TrataDigito(FILE *file, char *caractere, ListaDeTokens &lista_de_tokens){
+void TrataDigito(FILE *file, char *caractere, Token &token){
 
     //cria a variavel de controle "num"
     string num = "";
@@ -67,12 +32,12 @@ void TrataDigito(FILE *file, char *caractere, ListaDeTokens &lista_de_tokens){
         *caractere = fgetc(file);
     }
 
-    lista_de_tokens.simbolo.push(simbolo);
-    lista_de_tokens.lexema.push(num);
+    token.simbolo = simbolo;
+    token.lexema = num;
 
 }
 
-void TrataIDePalavraReservada(FILE *file, char *caractere, ListaDeTokens &lista_de_tokens){
+void TrataIDePalavraReservada(FILE *file, char *caractere, Token &token){
     //cria a variavel de controle "id"
     string id = "";
     string simbolo;
@@ -95,142 +60,142 @@ void TrataIDePalavraReservada(FILE *file, char *caractere, ListaDeTokens &lista_
         c = tolower(static_cast<unsigned char>(c));
     }
     //salva o lexema lido na lista de tokens
-    lista_de_tokens.lexema.push(id);
+    token.lexema = id;
 
     if(id == "programa"){
         simbolo = "sprograma";
-        lista_de_tokens.simbolo.push(simbolo);
+        token.simbolo = simbolo;
     }
     else if(id == "se"){
         simbolo = "sse";
-        lista_de_tokens.simbolo.push(simbolo);
+        token.simbolo = simbolo;
     }
     else if(id == "entao"){
         simbolo = "sentao";
-        lista_de_tokens.simbolo.push(simbolo);
+        token.simbolo = simbolo;
     }
     else if(id == "senao"){
         simbolo = "ssenao";
-        lista_de_tokens.simbolo.push(simbolo);
+        token.simbolo = simbolo;
     }
     else if(id == "enquanto"){
         simbolo = "senquanto";
-        lista_de_tokens.simbolo.push(simbolo);
+        token.simbolo = simbolo;
     }
     else if(id == "faca"){
         simbolo = "sfaca";
-        lista_de_tokens.simbolo.push(simbolo);
+        token.simbolo = simbolo;
     }
     else if(id == "inicio"){
         simbolo = "sinicio";
-        lista_de_tokens.simbolo.push(simbolo);
+        token.simbolo = simbolo;
     }
     else if(id == "fim"){
         simbolo = "sfim";
-        lista_de_tokens.simbolo.push(simbolo);
+        token.simbolo = simbolo;
     }
     else if(id == "escreva"){
         simbolo = "sescreva";
-        lista_de_tokens.simbolo.push(simbolo);
+        token.simbolo = simbolo;
     }
     else if(id == "leia"){
         simbolo = "sleia";
-        lista_de_tokens.simbolo.push(simbolo);
+        token.simbolo = simbolo;
     }
     else if(id == "var"){
         simbolo = "svar";
-        lista_de_tokens.simbolo.push(simbolo);
+        token.simbolo = simbolo;
     }
     else if(id == "inteiro"){
         simbolo = "sinteiro";
-        lista_de_tokens.simbolo.push(simbolo);
+        token.simbolo = simbolo;
     }
     else if(id == "booleano"){
         simbolo = "sbooleano";
-        lista_de_tokens.simbolo.push(simbolo);
+        token.simbolo = simbolo;
     }
     else if(id == "verdadeiro"){
         simbolo = "sverdadeiro";
-        lista_de_tokens.simbolo.push(simbolo);
+        token.simbolo = simbolo;
     }
     else if(id == "falso"){
         simbolo = "sfalso";
-        lista_de_tokens.simbolo.push(simbolo);
+        token.simbolo = simbolo;
     }
     else if(id == "procedimento"){
         simbolo = "sprocedimento";
-        lista_de_tokens.simbolo.push(simbolo);
+        token.simbolo = simbolo;
     }
     else if(id == "funcao"){
         simbolo = "sfuncao";
-        lista_de_tokens.simbolo.push(simbolo);
+        token.simbolo = simbolo;
     }
     else if(id == "div"){
         simbolo = "sdiv";
-        lista_de_tokens.simbolo.push(simbolo);
+        token.simbolo = simbolo;
     }
     else if(id == "ou"){
         simbolo = "sou";
-        lista_de_tokens.simbolo.push(simbolo);
+        token.simbolo = simbolo;
     }
     else if(id == "nao"){
         simbolo = "snao";
-        lista_de_tokens.simbolo.push(simbolo);
+        token.simbolo = simbolo;
     }
     else{
         simbolo = "sidentificador";
-        lista_de_tokens.simbolo.push(simbolo);
+        token.simbolo = simbolo;
     }
 
 
 }
 
-void TrataAtribuicao(FILE *file, char *caractere, ListaDeTokens &lista_de_tokens){
+void TrataAtribuicao(FILE *file, char *caractere, Token &token){
     *caractere = fgetc(file);
     string lexema, simbolo;
     if(*caractere == '='){
         lexema = ":=";
         simbolo = "satribuicao";
-        lista_de_tokens.lexema.push(lexema);
-        lista_de_tokens.simbolo.push(simbolo);
+        token.lexema = lexema;
+        token.simbolo = simbolo;
         *caractere = fgetc(file);
     }
     else{
         lexema = ":";
         simbolo = "sdoispontos";
-        lista_de_tokens.lexema.push(lexema);
-        lista_de_tokens.simbolo.push(simbolo);
+        token.lexema = lexema;
+        token.simbolo = simbolo;
     }
 }
 
-void TrataOPAritmetico(FILE *file, char *caractere, ListaDeTokens &lista_de_tokens){
+void TrataOPAritmetico(FILE *file, char *caractere, Token &token){
 
     string lexema, simbolo;
     // 43,45,42
     if(*caractere == 43){
         lexema = "+";
         simbolo = "smais";
-        lista_de_tokens.lexema.push(lexema);
-        lista_de_tokens.simbolo.push(simbolo);
+        token.lexema = lexema;
+        token.simbolo = simbolo;
     }
     else if(*caractere == 45){
         lexema = "-";
         simbolo = "smenos";
-        lista_de_tokens.lexema.push(lexema);
-        lista_de_tokens.simbolo.push(simbolo);
+        token.lexema = lexema;
+        token.simbolo = simbolo;
     }
     else if(*caractere == 42){
         lexema = "*";
         simbolo = "smult";
-        lista_de_tokens.lexema.push(lexema);
-        lista_de_tokens.simbolo.push(simbolo);
+        token.lexema = lexema;
+        token.simbolo = simbolo;
     }
 
     *caractere = fgetc(file);
 
 }
 
-void TrataOPRelacional(FILE *file, char *caractere, ListaDeTokens &lista_de_tokens){
+void TrataOPRelacional(FILE *file, char *caractere, Token &token){
 
     string lexema, simbolo;
     // 33, 60, 62, 61
@@ -238,8 +203,8 @@ void TrataOPRelacional(FILE *file, char *caractere, ListaDeTokens &lista_de_toke
         if ((*caractere = fgetc(file)) == 61) {
             lexema = "!=";
             simbolo = "sdif";
-            lista_de_tokens.lexema.push(lexema);
-            lista_de_tokens.simbolo.push(simbolo);
+            token.lexema = lexema;
+            token.simbolo = simbolo;
         }
         *caractere = fgetc(file);
     }
@@ -247,15 +212,15 @@ void TrataOPRelacional(FILE *file, char *caractere, ListaDeTokens &lista_de_toke
         if ((*caractere = fgetc(file)) == 61) {
             lexema = "<=";
             simbolo = "smenorig";
-            lista_de_tokens.lexema.push(lexema);
-            lista_de_tokens.simbolo.push(simbolo);
+            token.lexema = lexema;
+            token.simbolo = simbolo;
             *caractere = fgetc(file);
         }
         else {
             lexema = "<";
             simbolo = "smenor";
-            lista_de_tokens.lexema.push(lexema);
-            lista_de_tokens.simbolo.push(simbolo);
+            token.lexema = lexema;
+            token.simbolo = simbolo;
             *caractere = fgetc(file);
         }
     }
@@ -263,66 +228,66 @@ void TrataOPRelacional(FILE *file, char *caractere, ListaDeTokens &lista_de_toke
         if ((*caractere = fgetc(file)) == 61) {
             lexema = ">=";
             simbolo = "smaiorig";
-            lista_de_tokens.lexema.push(lexema);
-            lista_de_tokens.simbolo.push(simbolo);
+            token.lexema = lexema;
+            token.simbolo = simbolo;
             *caractere = fgetc(file);
         }
         else {
             lexema = ">";
             simbolo = "smaior";
-            lista_de_tokens.lexema.push(lexema);
-            lista_de_tokens.simbolo.push(simbolo);
+            token.lexema = lexema;
+            token.simbolo = simbolo;
             *caractere = fgetc(file);
         }
     }
     else if(*caractere == 61){
         lexema = "=";
         simbolo = "sig";
-        lista_de_tokens.lexema.push(lexema);
-        lista_de_tokens.simbolo.push(simbolo);
+        token.lexema = lexema;
+        token.simbolo = simbolo;
         *caractere = fgetc(file);
     }
 
 }
 
-void TrataPontuacao(FILE *file, char *caractere, ListaDeTokens &lista_de_tokens){
+void TrataPontuacao(FILE *file, char *caractere, Token &token){
 
     string lexema, simbolo;
     if(*caractere == 59){
         lexema = ";";
         simbolo = "spontoevirgula";
-        lista_de_tokens.lexema.push(lexema);
-        lista_de_tokens.simbolo.push(simbolo);
+        token.lexema = lexema;
+        token.simbolo = simbolo;
     }
     else if(*caractere == 46){
         lexema = ".";
         simbolo = "sponto";
-        lista_de_tokens.lexema.push(lexema);
-        lista_de_tokens.simbolo.push(simbolo);
+        token.lexema = lexema;
+        token.simbolo = simbolo;
     }
     else if(*caractere == 44){
         lexema = ",";
         simbolo = "svirgula";
-        lista_de_tokens.lexema.push(lexema);
-        lista_de_tokens.simbolo.push(simbolo);
+        token.lexema = lexema;
+        token.simbolo = simbolo;
     }
     else if(*caractere == 41){
         lexema = ")";
         simbolo = "sfechaparenteses";
-        lista_de_tokens.lexema.push(lexema);
-        lista_de_tokens.simbolo.push(simbolo);
+        token.lexema = lexema;
+        token.simbolo = simbolo;
     }
     else{
         lexema = "(";
         simbolo = "sabreparenteses";
-        lista_de_tokens.lexema.push(lexema);
-        lista_de_tokens.simbolo.push(simbolo);
+        token.lexema = lexema;
+        token.simbolo = simbolo;
     }
 
     *caractere = fgetc(file);
 }
 
-void TrataErro(FILE *file, char *caractere, ListaDeTokens &lista_de_tokens){
+void TrataErro(FILE *file, char *caractere, Token &token){
 
     if(*caractere == '}'){
         cout << endl << "ERRO LEXICAL NA LINHA " << contador << ": '"<< *caractere << "' tentativa de fechar comentario sem par de abertura";
@@ -335,89 +300,84 @@ void TrataErro(FILE *file, char *caractere, ListaDeTokens &lista_de_tokens){
     string lexema, simbolo;
     lexema = *caractere;
     simbolo = "serro";
-    lista_de_tokens.lexema.push(lexema);
-    lista_de_tokens.simbolo.push(simbolo);
+    token.lexema = lexema;
+    token.simbolo = simbolo;
 
     *caractere = fgetc(file);
 }
 
-void PegaToken(FILE *file, char *caractere, ListaDeTokens &lista_de_tokens){
+void PegaToken(FILE *file, char *caractere, Token &token){
+
+    int save_linha_abre_comentario;
+
+
+    //elimina comentarios e espacos
+    while ((*caractere == '{' || *caractere == ' ' || *caractere == '\n') && (*caractere != EOF)) {
+        if (*caractere == '{') {
+            save_linha_abre_comentario = contador;
+            // espera caractere de fechamento de comentario
+            while ((*caractere = fgetc(file)) != '}') {
+                if(*caractere == EOF){
+                    cout << endl << "ERRO LEXICAL NA LINHA " << save_linha_abre_comentario << ": comentario aberto nao foi fechado";
+                    break;
+                }
+            }
+        }
+        else if(*caractere == '\n'){
+            contador++;
+        }
+        // Ler pr�ximo caractere
+        *caractere = fgetc(file);
+    }
+
 
     //testa se o caractere esta entre '0' e '9'
     if(*caractere > 47 && *caractere < 58)
-        TrataDigito(file, caractere, lista_de_tokens);
+        TrataDigito(file, caractere, token);
 
     //testa se o caractere e uma letra entre 'A' e 'Z' ou entre 'a' e 'z'
     else if((*caractere > 64 && *caractere < 91) || (*caractere > 96 && *caractere < 123) || (*caractere == 95))
-        TrataIDePalavraReservada(file, caractere, lista_de_tokens);
+        TrataIDePalavraReservada(file, caractere, token);
 
     //testa se o caractere e igual a ':'
     else if(*caractere == 58)
-        TrataAtribuicao(file, caractere, lista_de_tokens);
+        TrataAtribuicao(file, caractere, token);
 
     //testa se o caractere pertence a {+,-,*}
     else if((*caractere == 43) || (*caractere == 45) || (*caractere == 42))
-        TrataOPAritmetico(file, caractere, lista_de_tokens);
+        TrataOPAritmetico(file, caractere, token);
 
     //testa se o caractere pertence a {!, <, >, =}
     else if((*caractere == 33) || (*caractere == 60) || (*caractere == 62) || (*caractere == 61))
-        TrataOPRelacional(file, caractere, lista_de_tokens);
+        TrataOPRelacional(file, caractere, token);
 
     //testa se o caractere pertence a {; , ( ) .}
     else if((*caractere == 59) || (*caractere == 44) || (*caractere == 40) || (*caractere == 41) || (*caractere == 46))
-        TrataPontuacao(file, caractere, lista_de_tokens);
-
-    else
-        TrataErro(file, caractere, lista_de_tokens);
-
-
-}
-
-void TrataEspacoComentario(FILE *file, char *caractere, ListaDeTokens &lista_de_tokens){
-
-    int save_linha_abre_comentario;
-    // Enquanto nao acabou o arquivo fonte
-    while (*caractere != EOF) {
-        //elimina comentarios e espacos
-        while ((*caractere == '{' || *caractere == ' ' || *caractere == '\n') && (*caractere != EOF)) {
-            if (*caractere == '{') {
-                save_linha_abre_comentario = contador;
-                // espera caractere de fechamento de comentario
-                while ((*caractere = fgetc(file)) != '}') {
-                    if(*caractere == EOF){
-                        cout << endl << "ERRO LEXICAL NA LINHA " << save_linha_abre_comentario << ": comentario aberto nao foi fechado";
-                        break;
-                    }
-                }
-            }
-            else if(*caractere == '\n'){
-                contador++;
-            }
-            // Ler pr�ximo caractere
-            *caractere = fgetc(file);
-        }
-
-        // Se caractere n�o for fim de arquivo
-        if (*caractere != EOF) {
-        //Logica de montegem de token - IMPLEMENTAR
-            PegaToken(file, caractere, lista_de_tokens);
-        }
-
+        TrataPontuacao(file, caractere, token);
+    else if(*caractere == EOF){
+        token.lexema = "EOF";
+        token.simbolo = "sfimarquivo";
     }
+    else
+        TrataErro(file, caractere, token);
+
+
 }
 
-
-void AnalisadorLexical(FILE *file) {
+void AnalisadorSintatico(FILE *file) {
 
     char caractere;
-    ListaDeTokens lista_de_tokens;
+    Token token;
 
     caractere = fgetc(file);
-    TrataEspacoComentario(file, &caractere, lista_de_tokens);
 
-    imprime_lista_token(lista_de_tokens);
+    do{
+        PegaToken(file, &caractere, token);
+        cout << endl << "lexema: " << token.lexema << " | Simbolo: " << token.simbolo;
+    }while(token.simbolo != "sfimarquivo");
 
 }
+
 
 void imprime_codigo_com_linhas(){
     FILE *arquivo_para_imprimir;
@@ -444,6 +404,7 @@ void imprime_codigo_com_linhas(){
     fclose(arquivo_para_imprimir);
 }
 
+
 int main() {
 
     FILE *file;
@@ -454,7 +415,7 @@ int main() {
         return 1;
     }
 
-    AnalisadorLexical(file);
+    AnalisadorSintatico(file);
     imprime_codigo_com_linhas();
 
 
